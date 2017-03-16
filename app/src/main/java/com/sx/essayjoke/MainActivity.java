61eb6973365d593a7f6a7240f1b80a457ddfc7f4
sunxin <sunxin0630@126.com>
2017-03-16 17:41:04 +0800

    add IOC baselibrary

--- app/src/main/java/com/sx/essayjoke/MainActivity.java
+++ app/src/main/java/com/sx/essayjoke/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sx.essayjoke;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
